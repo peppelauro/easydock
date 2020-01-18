f0f6506dcc229332f57e320c5a4234bcd1badae6
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2020-01-18 17:08:23 +0100

    Welcome page

--- public/index.php
+++ public/index.php
@@ -1,3 +1,79 @@
-<?php
-
-phpinfo();
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Easydock</title>
+        <link rel="stylesheet" href="https://allyoucan.cloud/cdn/fontawesome/5.11.2/css/all.css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+            .full-height {
+                height: 100vh;
+            }
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+            .position-ref {
+                position: relative;
+            }
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+            .content {
+                text-align: center;
+            }
+            .title {
+                font-size: 48px;
+            }
+            .subtitle {
+                font-size: 30px;
+            }
+            .mini {
+            	display: block;
+            	font-size: 16px;
+            }
+            .ultramini {
+            	font-size: 12px;
+            }
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                margin-bottom: 25px;
+            }
+            .m-b-md {
+                margin-bottom: 25px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title">
+                    <b>Easydock</b>
+                </div>
+                <div class="subtitle m-b-md">
+                   <span class="mini"><i class="fab fa-docker"></i> Docker LEMP integration script</span>
+                </div>
+                <div class="links">
+                    <a href="https://github.com/andreapollastri/easydock">By Andrea Pollastri</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
